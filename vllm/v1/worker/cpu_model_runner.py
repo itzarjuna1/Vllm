4e13c2f81c289b9c5cc76f83982abfdec0b39fa2
Andy Xie <andy.xning@gmail.com>
2025-08-27 00:36:47 +0800

    [Misc] refactor code by import as for torch._inductor.config
    Signed-off-by: Andy Xie <andy.xning@gmail.com>

--- vllm/v1/worker/cpu_model_runner.py
+++ vllm/v1/worker/cpu_model_runner.py
@@ -111,14 +111,14 @@ class CPUModelRunner(GPUModelRunner):
 
 @contextmanager
 def _set_global_compilation_settings(config: VllmConfig):
-    import torch._inductor.config
+    import torch._inductor.config as torch_inductor_config
 
     inductor_config = config.compilation_config.inductor_compile_config
     try:
         # Note: The MKLDNN and CPPGEMM backend requires freezing parameters.
-        freezing_value = torch._inductor.config.freezing
+        freezing_value = torch_inductor_config.freezing
         if inductor_config.get("max_autotune", False):
-            torch._inductor.config.freezing = True
+            torch_inductor_config.freezing = True
         yield
     finally:
-        torch._inductor.config.freezing = freezing_value
+        torch_inductor_config.freezing = freezing_value
